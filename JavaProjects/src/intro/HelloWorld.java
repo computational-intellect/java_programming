3742ac30b3719619c6d3988ec68062a5b221403d
James Wallace <jbwallace22@gmail.com>
2013-02-25 17:11:57 -0600

    Test Commit of Introduction to Java

---
+++ JavaProjects/src/intro/HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package intro;
+
+/**
+ *
+ * @author James
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
